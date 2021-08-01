99730cac2d61d553df72d00485f6b34af11d9031
sokripon <sokripon@gmail.com>
2021-08-01 22:31:25 +0200

    Added debug process_time message

--- ProjectName/app/middlewares/example.py
+++ ProjectName/app/middlewares/example.py
@@ -1,3 +1,5 @@
+import time
+
 from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.requests import Request
 
@@ -5,6 +7,9 @@ from starlette.requests import Request
 class CustomMiddleWare(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
         # Do stuff before request is handled
+        start = time.process_time()
         response = await call_next(request)
         # Do stuff after request is handled
+        print(time.process_time() - start, "seconds")
+
         return response
