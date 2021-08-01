9fc36597e9005e8abfa1e5447a1e26a77e9f1792
sokripon <sokripon@gmail.com>
2021-08-02 00:08:35 +0200

    Changed logging of main file

--- ProjectName/app/main.py
+++ ProjectName/app/main.py
@@ -1,4 +1,5 @@
 import logging
+import os
 from pathlib import Path
 
 import uvicorn
@@ -11,6 +12,8 @@ from ProjectName.app.managing.database import DB
 from ProjectName.app.middlewares.example import CustomMiddleWare
 from ProjectName.app.routers import page_routers, api_routers
 
+log = logging.getLogger("uvicorn")
+log.info(f"Uvicorn running on http://{settings.general.hostname}:{settings.general.port}")
 DB.Base.metadata.create_all(bind=DB.engine)
 
 app = FastAPI(title=settings.general.project_name, description="Template for an not so good api",
@@ -20,7 +23,12 @@ app.mount("/static", StaticFiles(directory=Path(__file__).parent.__str__() + "/s
 
 @app.on_event("shutdown")
 def shutdown():
-    print("EHE")
+    log.info("Application shutting down.")
+
+
+@app.on_event("startup")
+def kek():
+    log.info("Application startup complete.")
 
 
 from ProjectName.app.ws import ConnectionManager
@@ -39,7 +47,7 @@ if __name__ == "main":
     app.add_middleware(CustomMiddleWare)
     app.include_router(router=page_routers)
     app.include_router(router=api_routers)
-
+    log.info(f"Started server process [{os.getpid()}]")
 if __name__ == "__main__":
     uvicorn.run("main:app", host=settings.general.hostname, port=settings.general.port, reload=settings.general.reload,
-                log_level=logging.WARNING)
+                log_level=logging.WARNING, workers=2, use_colors=True)
