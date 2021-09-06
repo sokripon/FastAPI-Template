6b6da87d4423d29f454a969ac0ec2fb1da60fabd
sokripon <sokripon@gmail.com>
2021-09-06 14:54:09 +0200

    Fixed hardcoded websocket address

--- ProjectName/app/routers/pages/index.py
+++ ProjectName/app/routers/pages/index.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import Session
 from starlette.requests import Request
 
 from ProjectName.app.crud.v1.time_stamps import count_all_entries
+from ProjectName.app.managing.configs import settings
 from ProjectName.app.managing.database import DB
 from ProjectName.app.routers.pages import templates
 
@@ -14,4 +15,5 @@ homepage_root = APIRouter()
 @homepage_root.get("/")
 async def hello_world(request: Request, db: Session = Depends(DB.get_db)):
     return templates.TemplateResponse("index.html",
-                                      {"request": request, "time": datetime.now(), "amount": count_all_entries(db)})
+                                      {"request": request, "time": datetime.now(), "amount": count_all_entries(db),
+                                       "websocket": f"{settings.general.hostname}:{settings.general.port}"})
