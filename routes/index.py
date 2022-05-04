5bc950821cb94aeaea676c56aeea67b7c65f8569
sokripon <sokripon@gmail.com>
2022-05-04 09:21:05 +0200

    Added simple health route

--- routes/index.py
+++ routes/index.py
@@ -1,10 +1,15 @@
 from fastapi import APIRouter
 from starlette.requests import Request
 from utils.settings import templates
-
+from fastapi.responses import PlainTextResponse
 router_root = APIRouter()
 
 
 @router_root.get("/")
 def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request, "example": "Example"})
+
+
+@router_root.get("/health")
+def health(request: Request):
+    return PlainTextResponse("OK")
