2134591f8f555e8ee29cff57e9673b97a1b3090f
sokripon <sokripon@gmail.com>
2022-05-04 09:06:48 +0200

    Added example for mounting

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from starlette.staticfiles import StaticFiles
+
 from utils.logging import slogger
 from middlewares.logging import LoggingMiddleWare
 from routes.index import router_root
@@ -8,6 +10,7 @@ from utils.settings import setting
 app = FastAPI(title="Template", description="Template API", version="0.1.0", docs_url=setting.docs_url, redoc_url=setting.redoc_url, openapi_url=setting.openapi_url)
 app.add_middleware(LoggingMiddleWare)
 app.include_router(router=router_root)
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 slogger.init_logging()
 DB.create_myself()
