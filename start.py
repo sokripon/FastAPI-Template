b764084332f4ec7195c3ed464a7684da98f5bc2e
sokripon <sokripon@gmail.com>
2022-05-04 09:08:43 +0200

    Added simple print for start command

--- start.py
+++ start.py
@@ -7,5 +7,5 @@ if utils.settings.setting.debug:
     start_command.append("--reload")
 if utils.settings.setting.root_path:
     start_command.append(f"--root-path={utils.settings.setting.root_path}")
-
+print(f"Starting with command: {' '.join(start_command)}")
 subprocess.run(start_command)
